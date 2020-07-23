0dcdf1955f93cb0605fe7a98aa930438f34b36ac
Luke McKinstry <lukemckinstry@gmail.com>
2020-07-23 15:37:31 -0400

    presentation updates

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 import OuterContainer from './components/OuterContainer';
 
 function App() {
+  
+  var url = new URL(window.location.href);
+  var embedParam = url.searchParams.get('embed');
+  const embed = (embedParam === 'true')? true : false;
+  
   return (
     <div className="App">
-    	<div className="App-header">
+    	<div className={"App-header"+(embed?' embed':'')}>
           <div className="App-header-title">
           	DistrictBuilder
           </div>
--- src/components/MapboxGLMap.js
+++ src/components/MapboxGLMap.js
@@ -48,6 +48,8 @@ const MapboxGLMap = () => {
     mapboxgl.accessToken = 'pk.eyJ1IjoiYXphdmVhIiwiYSI6IkFmMFBYUUUifQ.eYn6znWt8NzYOa3OrWop8A';
     const initializeMap = ({ setMap, mapContainer }) => {
 
+      var [xmin,ymin,xmax,ymax] = [-24.026114086648512, -13.080309755245814, 20.700190980283185, 12.703790184833048];
+
       const map = new mapboxgl.Map({
         container: mapContainer.current,
         style: style,
@@ -55,27 +57,30 @@ const MapboxGLMap = () => {
         zoom: 4.7,
         bearing: 0,
         pitch: 0,
-        minzoom: 5,
-        maxzoom: 11,
-        hash: false
+        minZoom: 4.8,
+        maxZoom: 11,
+        hash: false,
       });
 
+      var nav = new mapboxgl.NavigationControl();
+      map.addControl(nav, 'top-left');
+      map.fitBounds([[xmax, ymax], [xmin, ymin]], { padding: 25 });
       initZoomToState(map)
-
       map.dragRotate.disable();
+      map.doubleClickZoom.disable();
       map.touchZoomRotate.disableRotation();
       map.on("load", () => {
         setMap(map);
-        loadMap(map);
-        map.resize();
+        loadMap(map);        
       });
+      window.history.replaceState({}, "", "/" );
     };
 
     if (!map) initializeMap({ setMap, mapContainer });
   }, [map]);
 
   const loadMap = function(map, layerName, styleMode) {
-    window.history.replaceState({}, "", "/" );
+    
     map.on('mousemove', 'cd-polygons', (e)=> {onMouseMove(e,map);});
     map.on('mouseleave', 'cd-polygons', ()=> {onMouseLeave(map);});
     map.on('click', 'cd-polygons', (e)=> {handleClickDistrict(e,map);});
@@ -291,9 +296,12 @@ const MapboxGLMap = () => {
         <div className="legend-title">
           {displayLabel}      
         </div>
+        
+        {display &&
         <div className='legend-clear' onClick={()=>handleLegendClearClick(selectedStateDistrictId)}>
           X
         </div>
+        }
       </div>
       )
   }
